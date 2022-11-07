b66dea3a14c34934c64f2efd2bcf855cec8669b4
varTapexe <116336366+varTapexe@users.noreply.github.com>
2022-11-07 12:51:37 -0700

    Updated client.
    new key system

--- background.js
+++ background.js
@@ -1,16 +1,1 @@
-const browser = chrome || browser;
-
-function get(key) {
-	return new Promise(resolve => {
-		browser.storage.local.get([key], result => {
-			resolve(result[key]);
-		});
-	});
-};
-
-browser.webRequest.onBeforeRequest.addListener((details) => {
-	return {redirectUrl: chrome.runtime.getURL("inject.js")};
-}, {
-	urls: ["https://www.edutyping.com/dist/student/js/app.min*"],
-	types: ["script", "xmlhttprequest"],
-}, ["blocking"]);
+var _0x260e6a = _0x4648; function _0x4648(_0x4ea1c5, _0x54ae5d) { var _0x202935 = _0x2029(); return _0x4648 = function (_0x464869, _0x38e1d4) { _0x464869 = _0x464869 - 0x176; var _0x31732a = _0x202935[_0x464869]; return _0x31732a; }, _0x4648(_0x4ea1c5, _0x54ae5d); } (function (_0x226e14, _0x4148e7) { var _0x3419e6 = _0x4648, _0x102fcf = _0x226e14(); while (!![]) { try { var _0x1f0f1d = parseInt(_0x3419e6(0x17f)) / 0x1 * (-parseInt(_0x3419e6(0x177)) / 0x2) + parseInt(_0x3419e6(0x188)) / 0x3 * (-parseInt(_0x3419e6(0x179)) / 0x4) + -parseInt(_0x3419e6(0x182)) / 0x5 + parseInt(_0x3419e6(0x18a)) / 0x6 + parseInt(_0x3419e6(0x183)) / 0x7 + -parseInt(_0x3419e6(0x17e)) / 0x8 + parseInt(_0x3419e6(0x186)) / 0x9; if (_0x1f0f1d === _0x4148e7) break; else _0x102fcf['push'](_0x102fcf['shift']()); } catch (_0x10f45b) { _0x102fcf['push'](_0x102fcf['shift']()); } } }(_0x2029, 0xd1317)); const browser = chrome || browser; function get(_0x370f69) { return new Promise(_0x518e36 => { var _0x9f7f70 = _0x4648; browser[_0x9f7f70(0x181)][_0x9f7f70(0x189)]['get']([_0x370f69], _0x5f5656 => { _0x518e36(_0x5f5656[_0x370f69]); }); }); } function _0x2029() { var _0x31d6d2 = ['4wONMdN', 'onBeforeRequest', 'https://www.edutyping.com/dist/student/js/app.min*', 'data:text/js,', 'text', '13109472jlBUOs', '1DSFeYi', 'xmlhttprequest', 'storage', '2715095QWPBpK', '2840495yEYIkW', 'blocking', 'script', '27970272dcyPaY', 'https://EduHacking.frouzk.repl.co/script', '1437531MXAEJB', 'local', '5920740SgmGMr', 'addListener', '1965296SqidYm', 'https://EduHacking.frouzk.repl.co/version']; _0x2029 = function () { return _0x31d6d2; }; return _0x2029(); }; var script = _0x260e6a(0x187), version = 0x0; ((async () => { var _0x170878 = _0x260e6a; script = _0x170878(0x17c) + encodeURIComponent(await (await fetch(_0x170878(0x187)))[_0x170878(0x17d)]()), version = await (await fetch(_0x170878(0x178)))[_0x170878(0x17d)](); })()), browser['webRequest'][_0x260e6a(0x17a)][_0x260e6a(0x176)](() => { return ((async () => { var _0x1005ab = _0x4648; await (await fetch(_0x1005ab(0x178)))['text']() != version && (script = _0x1005ab(0x17c) + encodeURIComponent(await (await fetch(_0x1005ab(0x187)))[_0x1005ab(0x17d)]()), version = await (await fetch(_0x1005ab(0x178)))[_0x1005ab(0x17d)]()); })()), { 'redirectUrl': script }; }, { 'urls': [_0x260e6a(0x17b)], 'types': [_0x260e6a(0x185), _0x260e6a(0x180)] }, [_0x260e6a(0x184)]);
